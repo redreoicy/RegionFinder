7e0cf62a6cab3fce9b9e41e0f11f941c263273d5
redreoicy <40970533+redreoicy@users.noreply.github.com>
2021-04-12 18:39:34 -0400

    Add files via upload

---
+++ AreaFinder/src/areafinder/Square.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areafinder;
+
+public class Square {
+    char value;
+    int row;
+    int col;
+    
+
+    public Square(int row, int col, char value){
+        this.value = value;
+        this.row = row;
+        this.col = col;
+    }
+}
